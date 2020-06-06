da1f2d06c8a60da525ea63adb9e6a7453417fccb
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-07 01:18:34 +0900

    sock: inline SELinux's sk_security to struct sock
    As we don't have to care about any other security mechanism than SELinux,
    inline its struct sk_security_struct into struct sock.
    
    This way, the kernel no longer has to allocate and de-allocate
    struct sk_security_struct, which happens extremely frequently under Android.
    
    Change-Id: If3370ade144f510b7105a15a570538d684669463
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [lazerl0rd: Adjust for Linux 4.19.]
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: acuicultor <ramoncuenca4@gmail.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -2000,7 +2000,8 @@ static void sock_copy(struct sock *nsk, const struct sock *osk)
 {
 	const struct proto *prot = READ_ONCE(osk->sk_prot);
 #ifdef CONFIG_SECURITY_NETWORK
-	void *sptr = nsk->sk_security;
+	struct sk_security_struct sksec;
+	memcpy(&sksec, nsk->sk_security, sizeof(sksec));
 #endif
 
 	/* If we move sk_tx_queue_mapping out of the private section,
@@ -2018,7 +2019,7 @@ static void sock_copy(struct sock *nsk, const struct sock *osk)
 	       prot->obj_size - offsetof(struct sock, sk_dontcopy_end));
 
 #ifdef CONFIG_SECURITY_NETWORK
-	nsk->sk_security = sptr;
+	memcpy(nsk->sk_security, &sksec, sizeof(sksec));
 	security_sk_clone(osk, nsk);
 #endif
 }
